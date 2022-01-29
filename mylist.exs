e4a69a4e071f4eb95c79a3fb67ac26768b5dada5
Eric Neumann <ene1@hey.com>
2022-01-30 08:14:52 +1000

    exc 7

--- mylist.exs
+++ mylist.exs
@@ -10,4 +10,30 @@ defmodule MyList do
 
   def map([], _func),             do: []
   def map([ head | tail ], func), do: [ func.(head) | map(tail, func) ]
+
+  def reduce([], value, _func), do: value
+  def reduce([head | tail], value, func), do: reduce(tail, func.(head, value), func)
+
+  def mapsum(list, value \\ 0, func)
+  def mapsum([], value, _func), do: value
+  def mapsum([head | tail], value, func) do
+    mapsum(tail, (func.(head) + value), func)
+  end
+
+  def max(list, index \\ 0)
+  def max([], index), do: index
+  def max([head|tail], index) when head > index do
+    index = head
+    MyList.max(tail, index)
+  end
+  def max([head|tail], index) when head <= index do
+    index
+    MyList.max(tail, index)
+  end
+
+  def caesar([], _shift), do: []
+  def caesar([head|tail], shift) do
+    head + shift
+    caesar(tail, shift)
+  end
 end
