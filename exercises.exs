af386b675959d2029c197a09434553e4dc318e05
Eric Neumann <ene1@hey.com>
2022-02-06 12:04:24 +1000

    redoing more exercises

--- exercises.exs
+++ exercises.exs
@@ -1,4 +1,4 @@
-defmodule Exercises do
+defmodule Ex do
 
   defmodule Times do
 
@@ -10,6 +10,19 @@ defmodule Exercises do
       num
       |> double
       |> double
+    end
+  end
+
+  defmodule BasicMaths do
+
+    def sum(0), do: 0
+    def sum(num) when num >= 1 do
+      num + sum(num-1)
+    end
+
+    def gcd(x, 0), do: x
+    def gcd(x, y) do
+      gcd(y, rem(x,y))
     end
   end
 end
